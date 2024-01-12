ff13753707ba403d76d0e129d5c01742e8e249b4
TessierV <5628@holbertonstudents.com>
2024-01-12 04:56:30 -0800

    test checker

---
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - inserts a number into a sorted singly linked list
+ * @head: pointer of the list
+ * @n: int to be inserted
+ * Return: newNode, or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int n)
+{
+    listint_t *newNode, *current, *prev;
+
+    if (head == NULL)
+        return (NULL);
+
+    newNode = malloc(sizeof(listint_t));
+    if (newNode == NULL)
+        return (NULL);
+
+    newNode->n = n;
+    newNode->next = NULL;
+
+    current = *head;
+    prev = NULL;
+
+    while (current != NULL && current->n < n)
+    {
+        prev = current;
+        current = current->next;
+    }
+
+    if (prev == NULL)
+    {
+        newNode->next = *head;
+        *head = newNode;
+        return (newNode);
+    }
+    else
+    {
+        prev->next = newNode;
+        newNode->next = current;
+    }
+
+    return (newNode);
+}
