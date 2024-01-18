0308e1a85d068e37c44d5229c4a9751709db0583
TessierV <5628@holbertonstudents.com>
2024-01-18 03:10:14 -0800

    initial commit for ex 1

---
+++ heap_insert/1-heap_insert.c
@@ -0,0 +1,45 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+    binary_tree_t *new_node, *parent;
+    if (!root)
+        return (NULL);
+
+    new_node = binary_tree_node(NULL, value);
+    if (!new_node)
+        return (NULL);
+
+    if (!*root)
+        return (*root = new_node, new_node);
+
+    parent = *root;
+    while (parent->left && parent->right)
+        parent = (binary_tree_height(parent->left) <= binary_tree_height(parent->right)) ? parent->left : parent->right;
+
+    new_node->parent = parent;
+    (parent->left) ? (parent->right = new_node) : (parent->left = new_node);
+
+    heapify_up(new_node);
+
+    return (new_node);
+}
+
+int binary_tree_height(const binary_tree_t *tree)
+{
+    int left_height, right_height;
+    if (!tree)
+        return (0);
+
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
+
+    return (1 + (left_height > right_height ? left_height : right_height));
+}
+
+void heapify_up(binary_tree_t *node)
+{
+    while (node->parent && node->n > node->parent->n)
+        node->n ^= node->parent->n, node->parent->n ^= node->n, node->n ^= node->parent->n, node = node->parent;
+}
