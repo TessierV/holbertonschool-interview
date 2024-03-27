de3445a8d4daf0404d0007228996e0d14003e02f
TessierV <5628@holbertonstudents.com>
2024-03-27 01:47:28 -0700

    fix betty for header

--- menger/menger.h
+++ menger/menger.h
@@ -5,14 +5,20 @@
 #include <math.h>
 
 /**
- * header - Function
+ * check_char - Checks space for block.
+ * @column: Is the column.
+ * @row: Is the row.
+ *
+ * Return: # or space.
+ */
+char check_char(int column, int row);
+
+/**
+ * menger - Function that draws a 2D Menger Sponge.
  * @level: Is the level of the Menger Sponge to draw.
  *
  * Return: Void.
  */
-char check_char(int column, int row);
-
-
 void menger(int level);
 
 #endif
