64464510e9642ed49971238f5dbdbdcd62da4c60
TessierV <5628@holbertonstudents.com>
2024-04-16 02:57:27 -0700

    fix checked value, remove comment, try to fix betty checker annoying

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -9,41 +9,30 @@
  * @list: A pointer to the head of the skip list.
  * @value: The value to search for.
  *
- * Return: A pointer
+ * Return: A pointer to the first newNode where value is located,
  * or NULL if value is not present or list is NULL.
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	if (!list)
 		return (NULL);
-
 	skiplist_t *newNode = list, *express = list->express, *last = newNode;
-	while (express != NULL && express->n < value)
+
+	while (express && express->n < value)
 	{
 		printf(CHECKED_VALUE, express->index, express->n);
 		newNode = express;
 		express = express->express;
 	}
+	printf(FOUND_BETWEEN_INDEXES, newNode->index, (express ? express->index : last->index));
 
-	if (express != NULL)
-	{
-		printf(CHECKED_VALUE, express->index, express->n);
-	}
-	else
-	{
-		last = newNode;
-		while (last->next != NULL)
-			last = last->next;
-		printf(CHECKED_VALUE, last->index, last->n);
-	}
-	printf(FOUND_BETWEEN_INDEXES,
-			newNode->index, (express ? express->index : last->index));
-	while (newNode != NULL && newNode->n < value)
+	while (newNode && newNode->n < value)
 	{
 		printf(CHECKED_VALUE, newNode->index, newNode->n);
 		newNode = newNode->next;
 	}
-	if (newNode != NULL && newNode->n == value)
+
+	if (newNode && newNode->n == value)
 	{
 		printf(CHECKED_VALUE, newNode->index, newNode->n);
 		return (newNode);
