18efaccc697514d641d14a0ae357ec57a0a7c0b8
TessierV <5628@holbertonstudents.com>
2024-04-23 01:12:35 -0700

    test checker

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,28 +1,35 @@
 #!/usr/bin/node
 
 const request = require('request');
-
 const movieId = process.argv[2];
+
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-request(url, (error, response, body) => {
+function getCharacterName(url) {
+  return new Promise((resolve, reject) => {
+    request(url, { json: true }, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(body.name);
+      }
+    });
+  });
+}
+
+request(url, { json: true }, async (error, response, body) => {
   if (error) {
     console.error(error);
     return;
   }
 
-  const film = JSON.parse(body);
-  const charactersUrls = film.characters;
+  const characterUrls = body.characters;
+  const characterPromises = characterUrls.map(url => getCharacterName(url));
 
-  charactersUrls.forEach((characterUrl) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-
-      const character = JSON.parse(body);
-      console.log(character.name);
-    });
-  });
+  try {
+    const characterNames = await Promise.all(characterPromises);
+    characterNames.forEach(name => console.log(name));
+  } catch (error) {
+    console.error(error);
+  }
 });
