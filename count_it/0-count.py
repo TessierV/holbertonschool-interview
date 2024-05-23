71ec9af76b6390d63f470a9096370beefde537a2
TessierV <5628@holbertonstudents.com>
2024-05-23 06:42:14 -0700

    undo

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -3,36 +3,39 @@
 from requests import get
 
 
-def count_words(subreddit, word_list, after="", word_dic=None):
+def count_words(subreddit, word_list, after="", word_dic={}):
     """ function that queries the Reddit API """
 
-    if word_dic is None:
-        word_dic = {word: 0 for word in word_list}
-
+    if not word_dic:
+        for word in word_list:
+            word_dic[word] = 0
     if after is None:
-        word_list = sorted(word_dic.items(), key=lambda x: (-x[1], x[0]))
-        for word, count in word_list:
-            if count:
-                print(f"{word.lower()}: {count}")
+        word_list = [[key, value] for key, value in word_dic.items()]
+        word_list = sorted(word_list, key=lambda x: (-x[1], x[0]))
+        for word in word_list:
+            if word[1]:
+                print("{}: {}".format(word[0].lower(), word[1]))
         return None
-
-    endpoint = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    endpoint = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {'user-agent': 'my-app/0.0.1'}
     params = {'limit': 100, 'after': after}
-
     resp = get(endpoint, headers=headers, params=params, allow_redirects=False)
     if resp.status_code != 200:
         return None
-
     try:
-        data = resp.json().get("data")
+        json = resp.json()
+    except ValueError:
+        return None
+    try:
+        data = json.get("data")
         after = data.get("after")
         children = data.get("children")
         for child in children:
-            title = child.get("data").get("title").lower().split()
+            post = child.get("data")
+            title = post.get("title")
+            lower = [t.lower() for t in title.split(' ')]
             for word in word_list:
-                word_dic[word] += title.count(word.lower())
+                word_dic[word] += lower.count(word.lower())
     except Exception:
         return None
-
     count_words(subreddit, word_list, after, word_dic)
