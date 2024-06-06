a280fea22d348146724eeb12c8589d9f0e18ddca
TessierV <5628@holbertonstudents.com>
2024-06-06 07:45:17 -0700

    test betty

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -11,14 +11,14 @@
  */
 int is_digit(char *s)
 {
-    int i;
+	int i;
 
-    for (i = 0; s[i]; i++)
-    {
-        if (!isdigit(s[i]))
-            return (0);
-    }
-    return (1);
+	for (i = 0; s[i]; i++)
+	{
+		if (!isdigit(s[i]))
+			return (0);
+	}
+	return (1);
 }
 
 /**
@@ -28,37 +28,37 @@ int is_digit(char *s)
  */
 void multiply(char *num1, char *num2)
 {
-    int len_num1 = strlen(num1);
-    int len_num2 = strlen(num2);
-    int len_result = len_num1 + len_num2;
-    int *result = calloc(len_result, sizeof(int));
-    int i, j;
+	int len_num1 = strlen(num1);
+	int len_num2 = strlen(num2);
+	int len_result = len_num1 + len_num2;
+	int *result = calloc(len_result, sizeof(int));
+	int i, j;
 
-    for (i = len_num1 - 1; i >= 0; i--)
-    {
-        for (j = len_num2 - 1; j >= 0; j--)
-        {
-            int mul = (num1[i] - '0') * (num2[j] - '0');
-            int sum = mul + result[i + j + 1];
-            result[i + j] += sum / 10;
-            result[i + j + 1] = sum % 10;
-        }
-    }
+	for (i = len_num1 - 1; i >= 0; i--)
+	{
+		for (j = len_num2 - 1; j >= 0; j--)
+		{
+			int mul = (num1[i] - '0') * (num2[j] - '0');
+			int sum = mul + result[i + j + 1];
+			result[i + j] += sum / 10;
+			result[i + j + 1] = sum % 10;
+		}
+	}
 
-    i = 0;
-    while (i < len_result && result[i] == 0)
-        i++;
+	i = 0;
+	while (i < len_result && result[i] == 0)
+		i++;
 
-    if (i == len_result)
-        printf("0");
-    else
-    {
-        while (i < len_result)
-            printf("%d", result[i++]);
-    }
-    printf("\n");
+	if (i == len_result)
+		printf("0");
+	else
+	{
+		while (i < len_result)
+			printf("%d", result[i++]);
+	}
+	printf("\n");
 
-    free(result);
+	free(result);
 }
 
 /**
@@ -70,19 +70,19 @@ void multiply(char *num1, char *num2)
  */
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return (98);
-    }
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-    if (!is_digit(argv[1]) || !is_digit(argv[2]))
-    {
-        printf("Error\n");
-        return (98);
-    }
+	if (!is_digit(argv[1]) || !is_digit(argv[2]))
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-    multiply(argv[1], argv[2]);
+	multiply(argv[1], argv[2]);
 
-    return (0);
+	return (0);
 }
