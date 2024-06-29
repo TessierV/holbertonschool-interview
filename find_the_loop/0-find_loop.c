7c7a245feec8d4ae4209307a8ee213ba54981db8
TessierV <5628@holbertonstudents.com>
2024-06-29 12:40:47 -0700

    betty fix

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -10,26 +10,26 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *slow = head, *fast = head;
+	listint_t *slow = head, *fast = head;
 
-    while (fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-        if (fast == slow)
-            break;
-    }
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (fast == slow)
+			break;
+	}
 
-    if (!fast || !fast->next)
-        return (NULL);
+	if (!fast || !fast->next)
+		return (NULL);
 
-    slow = head;
+	slow = head;
 
-    while (slow != fast)
-    {
-        slow = slow->next;
-        fast = fast->next;
-    }
+	while (slow != fast)
+	{
+		slow = slow->next;
+		fast = fast->next;
+	}
 
-    return (slow);
+	return (slow);
 }
