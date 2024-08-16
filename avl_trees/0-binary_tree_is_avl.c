bf844d7191971f5e6ab7da61c4efeaf28eeddcb7
TessierV <5628@holbertonstudents.com>
2024-08-16 07:11:49 -0700

    fix betty

--- avl_trees/0-binary_tree_is_avl.c
+++ avl_trees/0-binary_tree_is_avl.c
@@ -8,15 +8,15 @@
  */
 int height(const binary_tree_t *tree)
 {
-    int left_height, right_height;
+	int left_height, right_height;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    left_height = height(tree->left);
-    right_height = height(tree->right);
+	left_height = height(tree->left);
+	right_height = height(tree->right);
 
-    return (left_height > right_height ? left_height + 1 : right_height + 1);
+	return (left_height > right_height ? left_height + 1 : right_height + 1);
 }
 
 /**
@@ -29,14 +29,14 @@ int height(const binary_tree_t *tree)
  */
 int is_bst(const binary_tree_t *tree, int min, int max)
 {
-    if (tree == NULL)
-        return (1);
+	if (tree == NULL)
+		return (1);
 
-    if (tree->n <= min || tree->n >= max)
-        return (0);
+	if (tree->n <= min || tree->n >= max)
+		return (0);
 
-    return (is_bst(tree->left, min, tree->n) &&
-            is_bst(tree->right, tree->n, max));
+	return (is_bst(tree->left, min, tree->n) &&
+			is_bst(tree->right, tree->n, max));
 }
 
 /**
@@ -47,22 +47,22 @@ int is_bst(const binary_tree_t *tree, int min, int max)
  */
 int is_avl(const binary_tree_t *tree)
 {
-    int left_height, right_height, balance_factor;
+	int left_height, right_height, balance_factor;
 
-    if (tree == NULL)
-        return (1);
+	if (tree == NULL)
+		return (1);
 
-    if (!is_bst(tree, INT_MIN, INT_MAX))
-        return (0);
+	if (!is_bst(tree, INT_MIN, INT_MAX))
+		return (0);
 
-    left_height = height(tree->left);
-    right_height = height(tree->right);
-    balance_factor = left_height - right_height;
+	left_height = height(tree->left);
+	right_height = height(tree->right);
+	balance_factor = left_height - right_height;
 
-    if (balance_factor > 1 || balance_factor < -1)
-        return (0);
+	if (balance_factor > 1 || balance_factor < -1)
+		return (0);
 
-    return (is_avl(tree->left) && is_avl(tree->right));
+	return (is_avl(tree->left) && is_avl(tree->right));
 }
 
 /**
@@ -73,8 +73,8 @@ int is_avl(const binary_tree_t *tree)
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    return (is_avl(tree));
+	return (is_avl(tree));
 }
